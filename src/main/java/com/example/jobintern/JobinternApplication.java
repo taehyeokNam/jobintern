0fe55f400bbaee4296d24e4b931e2f6ae486fd1a
taehyeok Nam <navyth91@naver.com>
2025-01-18 21:01:40 +0900

    first commit

---
+++ src/main/java/com/example/jobintern/JobinternApplication.java
@@ -0,0 +1,13 @@
+package com.example.jobintern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobinternApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobinternApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jobintern/JobinternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jobintern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobinternApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
